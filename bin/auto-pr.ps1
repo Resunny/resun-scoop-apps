1b51747ef099055a023937b43b2a9d491c8505bf
Resun <Resun@email.com>
2023-07-17 20:29:57 +0800

    Modify README.md and bin/auto-pr.ps1.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Resunny/resun-scoop-apps:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
